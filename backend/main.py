ee2372c49759099db734c7068dab938ac382bce6
rbherva289-cmyk <rbherva289@gmail.com>
2026-08-08 02:07:50 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,23 +2,5 @@ from fastapi import FastAPI
 from market_data import get_market_data
 from sentiment import analyze_sentiment
 from ai_model import predict
-
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "AI Market Predictor"}
-
-@app.get("/predict")
-def prediction():
-    market = get_market_data()
-    sentiment = analyze_sentiment()
-    result = predict()
-
-    return {
-        "market": market,
-        "sentiment": sentiment,
-        "prediction": result
-    }
 from logger import log_message
 from security import check_api_key
