19a54393c15ee17e90d46a5e57fec04cc828b422
rbherva289-cmyk <rbherva289@gmail.com>
2026-08-08 01:44:02 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="AI Market Predictor")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+app = FastAPI()
 
 @app.get("/")
 def home():
-    return {
-        "message": "AI Market Predictor Running"
-    }
-
-
-@app.get("/predict")
-def predict():
-    return {
-        "symbol": "NIFTY",
-        "signal": "BUY",
-        "confidence": "87%"
-    }
+    return {"message": "AI Market Predictor"}
