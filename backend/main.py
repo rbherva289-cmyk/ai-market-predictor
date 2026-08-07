75c262994e554c23f2ad4b382150a943e1e517a1
rbherva289-cmyk <rbherva289@gmail.com>
2026-08-07 23:19:21 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -26,3 +26,23 @@ def predict():
         "signal": "BUY",
         "confidence": "87%"
     }
+from fastapi import FastAPI
+from indicators import calculate_rsi
+from sentiment import analyze_sentiment
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "AI Market Predictor"}
+
+@app.get("/market")
+def market():
+    sentiment = analyze_sentiment()
+
+    return {
+        "signal": "BUY",
+        "rsi": 65,
+        "macd": "Bullish",
+        "sentiment": sentiment
+    }
