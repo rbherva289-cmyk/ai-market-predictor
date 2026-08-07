744040afa122e8581e261e99b134e7602226a6f6
rbherva289-cmyk <rbherva289@gmail.com>
2026-08-08 01:02:24 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,18 @@
-backend/app.py
+from fastapi import FastAPI
+from predictor import predict_market
+from signal_engine import generate_signal
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "running"}
+
+@app.get("/predict")
+def prediction():
+    result = predict_market()
+    signal = generate_signal(result)
+    return {
+        "prediction": result,
+        "signal": signal
+    }
